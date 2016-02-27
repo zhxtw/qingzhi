a5238776a358786b5f872e268bc0fbf9d40928ff
Jerry Cheung <571339406@qq.com>
2016-02-27 09:50:24 +0800

    Revert "修改密码"

--- shownav.php
+++ shownav.php
@@ -1,46 +1,63 @@
-<nav class="navbar navbar-default">
+<style type="text/css">
+	hr {
+		border-top-color:#ddd;
+	}
+	.navbar-nav .open .dropdown-menu > li > a {
+    	font-size: 14px;
+  }
+	.h1, .h2, .h3, .h4, body, h1, h2, h3, h4, h5, h6 {
+    font-family: Microsoft YaHei,Roboto,Helvetica,Arial,sans-serif !important;
+    font-weight: 500 !important;
+	}
+</style>
+<nav class="navbar navbar-success">
   <div class="container-fluid">
-    <!-- Brand and toggle get grouped for better mobile display -->
     <div class="navbar-header">
-      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#defaultNavbar1"><span class="sr-only">Toggle navigation</span><span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span></button>
-      <a class="navbar-brand" href="#">执信团委青志后台管理&nbsp;<span class="label label-danger">Beta</span></a></div>
-    <!-- Collect the nav links, forms, and other content for toggling -->
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#defaultNavbar1"><span class="sr-only"></span><span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span></button>
+      <a class="navbar-brand" href="#">执信青年志愿者协会&nbsp;<span class="label label-danger">Beta</span></a></div>
+
     <div class="collapse navbar-collapse" id="defaultNavbar1">
-      <ul class="nav navbar-nav">
-        <li class="dropdown"><a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">报名信息管理<span class="caret"></span></a>
-          <ul class="dropdown-menu" role="menu">
-            <li><a href="manage.php">报名管理</a></li>
-						<li><a href="assign.php">分配时段</a></li>
-            <li><a href="change.php">调换人地</a></li>
-            <li><a href="addmany.php">批量增加</a></li>
-          </ul>
-        </li>
-        <li class="dropdown"><a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">地点时段管理<span class="caret"></span></a>
-          <ul class="dropdown-menu" role="menu">
-            <li><a href="manageloc.php">地点管理</a></li>
-            <li><a href="managetime.php">时段管理</a></li>
-          </ul>
-        </li>
-        <li class="dropdown"><a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">公告通知管理<span class="caret"></span></a>
-          <ul class="dropdown-menu" role="menu">
-            <li><a href="managepost.php">发布公告</a></li>
-            <li><a href="manageblame.php">意见反馈查看</a></li>
-          </ul>
-        </li>
-      </ul>
-	  <ul class="nav navbar-nav navbar-right">
-		  <li class="dropdown"><a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false"><img style="width:18px;border-radius:9px;" src="../img/user.png"></a>
-          <ul class="dropdown-menu" role="menu">
-          	<li><a href="#"><b><?php echo($_SESSION['adminname']); ?></b></a></li>
-            <li><a href="personal.php">个人信息</a></li>
-            <li><a href="toModifyPW.php">修改密码</a></li>
-            <li class="divider"></li>
-            <li><a href="logout.php">退出登录</a></li>
-          </ul>
-          </li>
-      </ul>
+      <ul class="nav navbar-nav" id="nav1left"></ul>
+      <ul class="nav navbar-nav navbar-right" id="nav1right"></ul>
     </div>
-    <!-- /.navbar-collapse -->
   </div>
-  <!-- /.container-fluid -->
 </nav>
+<script src="js/jquery-1.11.2.min.js"></script>
+<script>
+	l=$.ajax({async:false,url:"nav.json",dataType:"json",type:"GET",
+		success: function(){}
+	});
+	if(l.statusText!="OK"){
+		alert("导航栏信息加载失败！\n请刷新页面重试。");
+	}
+	nav=eval("("+l.responseText+")");
+
+	function retE(ja){
+		if(ja.submenu){
+			tmp='<li class="dropdown"><a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">' + ja.name + '<span class="caret"></span></a><ul class="dropdown-menu" role="menu">';
+			for(i in ja.submenu){
+				tmp+=retE(ja.submenu[i]);
+			}
+			tmp+='</ul></li>';
+		} else {
+			tmp=(ja.divide ? '<li class="divider"></li>' : '') + '<li' + (isActive(ja) ? ' class="active"' : '') +'><a href="' + ja.href + '">' + ja.name + '</a></li>';
+		}
+		return tmp;
+	}
+	function isActive(ja){
+		if(ja.href=="<?php echo($_SERVER['PHP_SELF']); ?>"){
+			return 1;
+		}
+		return 0;
+	}
+	function appendNav(){
+		for(i in nav){
+			if(nav[i].right){
+				$("#nav1right").append(retE(nav[i]));
+			}else{
+				$("#nav1left").append(retE(nav[i]));
+			}
+		}
+		$.material.init();
+	}
+</script>
