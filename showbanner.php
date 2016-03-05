5a32027de23bba67772a009c16fcc0829393b56d
zhangjingye03 <934526987@qq.com>
2016-03-05 21:07:56 +0800

    Change the project link
    20160305

--- showbanner.php
+++ showbanner.php
@@ -4,7 +4,7 @@
     <div class="text-center col-md-6 col-md-offset-3">
       <h4>共青团广州市执信中学委员会</h4>
       <p>Copyright &copy; 2015-2016 &middot; All Rights Reserved</p><p><a href="http://weibo.com/zhxtw/" >执信团委</a> <a href="http://weibo.com/zhxtwit/" >信息部</a></p>
-      <p><a href="https://github.com/zhangjingye03/zhxtw">开放源代码及其协议声明</a></p>
+      <p><a href="https://github.com/zhxtw/qingzhi">开放源代码及其协议声明</a></p>
     </div>
   </div>
   </div>
