a4d946649135e2070ba71728898329d75a4c9b1a
zhangjingye03 <934526987@qq.com>
2016-03-09 18:55:52 +0800

    Optimize the showing style
    20160309

--- showbanner.php
+++ showbanner.php
@@ -6,14 +6,14 @@
       <h4>共青团广州市执信中学委员会</h4>
       <p>Copyright &copy; 2015-2016 &middot; All Rights Reserved</p><p><a href="http://zhxhs.net">执信</a> <a href="http://weibo.com/zhxtw/" >团委</a> <a href="http://weibo.com/zhxtwit/" >信息部</a></p>
       <p><a href="https://github.com/zhxtw/qingzhi">开放源代码及其协议声明</a></p>
-      <p>代码版本：<a href="#" id="ver" style="font-family:Consolas;color:blue"></a></p><p>Commited by <a href="#" id="author" style="color:blue"></a></p>
-      <script>
-        $.ajax({type:"GET",url:"https://api.github.com/repos/zhxtw/qingzhi/commits/master",dataType:"json",success:function(got){
-          $("#ver").html(got.sha.substr(0,8));$("#ver")[0].href=got.html_url;
-          $("#author").html(got.author.login);$("#author")[0].href=got.author.html_url;
-        }});
-      </script>
     </div>
   </div>
   </div>
 <hr>
+<p class="text-center" style="color:gray">代码版本：<a href="#" id="ver" style="font-family:Consolas;color:blue"></a>, Last commited by <a href="#" id="author" style="color:blue"></a></p>
+<script>
+  $.ajax({type:"GET",url:"https://api.github.com/repos/zhxtw/qingzhi/commits/master",dataType:"json",success:function(got){
+    $("#ver").html("git@"+got.sha.substr(0,8));$("#ver")[0].href=got.html_url;
+    $("#author").html(got.author.login);$("#author")[0].href=got.author.html_url;
+  }});
+</script>
