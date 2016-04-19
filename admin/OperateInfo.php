684af6fa57de5cd4ec7ca05cd0a4cae6ef489286
Jerry Cheung <571339406@qq.com>
2016-04-19 22:18:18 +0800

    Get Ready For Managing News 为管理新闻做准备
    打算把操作数据放在一个页面
    
    Not successful,
    Must not Clone/Fork!

--- admin/OperateInfo.php
+++ admin/OperateInfo.php
@@ -1,11 +1,12 @@
 <?php
 $flag=true;//Verify to_pdo.php
-require_once("../to_pdo.php"); 
-$FBid=$_POST['fbid'];//需要标注的意见ID
+require_once("../to_pdo.php");
+$type=$_POST['type'];//需要操作的内容类型(eg.News/FeedBack)
+$id=$_POST['id'];//需要操作的信息ID
 $Todo=$_POST['todo'];//需要标注的内容
 $q=array(); $qi=0;
-$astr=explode(",",$FBid);
-if($Todo=="已删除"){$base="DELETE from feedback where ";}//删除记录
+$astr=explode(",",$id);
+if($Todo=="已删除"){$base="DELETE from '{$type}' where ";}//删除记录
 else{$base="UPDATE feedback SET status='{$Todo}' where ";}//修改记录
 
 //Add Where
