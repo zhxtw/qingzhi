5479d76f878511ecb5d96379f80ec825ac10495f
张靖业 <934526987@qq.com>
2016-07-02 00:22:20 +0800

    Minor update

--- to_json.php
+++ to_json.php
@@ -1,6 +1,7 @@
 <?php
 	header("content-type:text/html;charset=utf-8");
-	$a=file_get_contents("location.json");
+	$dir = dirname(__FILE__); //获取当前文件的目录绝对路径，防止被/admin内文件require的时候路径被切换到/admin
+	$a = file_get_contents( $dir . "/location.json" );
 	$a=json_decode($a);
 	$alldisabled=$a->alldisabled;
 	$a=$a->loc;
