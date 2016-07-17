9c757f298f301dada652b1a7b4ef58deca7438f6
张靖业 <934526987@qq.com>
2016-07-17 15:00:26 +0800

    Fix sorting issue
    ~ 修复排序问题

--- admin/getRes.php
+++ admin/getRes.php
@@ -49,20 +49,21 @@
 	if(isset($_POST['sort']) && !empty($_POST['sort'])){
 		$sort=$_POST['sort'];
 		switch($sort){
+			//汉字按拼音排序：转换为gbk编码再排
 			case "姓名":
-				$willsort="name";break;
+				$willsort="CONVERT(name USING gbk) ASC";break;
 			case "班别":
-				$willsort="classno";break;
+				$willsort="CONVERT(tworone USING gbk) DESC, classno ASC";break; //先排年级再排班级
 			case "年级":
-				$willsort="tworone";break;
+				$willsort="CONVERT(tworone USING gbk) ASC, classno DESC";break; //和上面的反过来
 			case "志愿点":
-				$willsort="loc_name";break;
+				$willsort="CONVERT(loc_name USING gbk) ASC";break;
 			case "时段":
-				$willsort="times";break;
+				$willsort="CONVERT(times USING gbk) ASC";break;
 			case "报名时间":
-				$willsort="datetime";break;
+				$willsort="datetime ASC";break;
 			case "通过状态":
-				$willsort="go";break;
+				$willsort="go ASC";break;
 			default:
 				$willsort="1";
 		}
@@ -74,7 +75,7 @@
 			而ORDER BY 跟的是字段名 不需要引号！
 			此处已经把传入参数重新对应字段名，不存在注入问题，可以直接查询。
 		*/
-		$query.=" ORDER BY $willsort ASC";
+		$query.=" ORDER BY $willsort";
 	}
 
 	$query.=" LIMIT ?,?";
