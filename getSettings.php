09c046bd34ecbda4bced370cde056c54c6ca357e
张靖业 <934526987@qq.com>
2016-07-17 14:57:41 +0800

    Update settings
    ~ 更新设置相关代码

--- getSettings.php
+++ getSettings.php
@@ -12,7 +12,8 @@
 * @param what    设置项名称
 */
   function getSettings($what) {
-    $jsonset = json_decode( file_get_contents( "settings.json" ) );
+    $dir = dirname(__FILE__);
+    $jsonset = json_decode( file_get_contents( $dir . "/settings.json" ) );
     if( !isset( $jsonset->$what ) ) {
       return null;
     } else {
